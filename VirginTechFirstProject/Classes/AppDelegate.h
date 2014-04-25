ca495300128495e07ba3ad95b5fc9c98c1f62db9
VirginTech LLC <virgintech@MacProRetina.local>
2014-04-25 14:43:15 +0900

    Initial Commit

---
+++ VirginTechFirstProject/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  VirginTechFirstProject
+//
+//  Created by VirginTech LLC. on 2014/04/25.
+//  Copyright VirginTech LLC. 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ VirginTechFirstProject/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VirginTechFirstProject
+//
+//  Created by VirginTech LLC. on 2014/04/25.
+//  Copyright VirginTech LLC. 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
